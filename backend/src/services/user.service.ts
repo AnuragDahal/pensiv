218444a20379b2ce4150508f2e8e199288b8dd3b
AnuragDahal <freefirepros2020@gmail.com>
2025-01-20 20:59:22 +0545

    feat: add password hashing and JWT token generation to user service

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -1,6 +1,9 @@
-import { User } from "@/models/users.model";
+import { User, userSchema } from "@/models/users.model";
 import { IUser } from "@/types/user";
 import { Types } from "mongoose";
+import bcrypt from "bcrypt";
+import { env } from "@/env";
+import jwt from "jsonwebtoken";
 
 export const getUsers = () => User.find();
 export const getUserById = (id: string | Types.ObjectId) => User.findById(id);
@@ -15,3 +18,47 @@ export const getUserByEmail = (email: string) => User.findOne({ email });
 export const deleteUserById = (id: string) => User.findByIdAndDelete(id);
 export const deleteUserByEmail = (email: string) =>
   User.findOneAndDelete({ email });
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) next();
+  const hashedPassword = await bcrypt.hash(this.password, 12);
+  this.password = hashedPassword;
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (
+  candidatePassword: string
+) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  if (!env.ACCESS_TOKEN_SECRET) {
+    throw new Error("ACCESS_TOKEN_SECRET is not defined");
+  }
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      name: this.name,
+    },
+    env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: "10h",
+    }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  if (!env.REFRESH_TOKEN_SECRET) {
+    throw new Error("REFRESH_TOKEN_SECRET is not defined");
+  }
+  return jwt.sign(
+    {
+      email: this.email,
+    },
+    env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: "7d",
+    }
+  );
+};
